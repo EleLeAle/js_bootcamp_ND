c1041e4789472a378610a97001d112b271c8318e
EleLeAle <felenfrancesca@icloud.com>
2025-03-10 02:23:00 -0400

    Tic-tac-toe works, but need to fix: when game is over show this and disable display for a couple seconds

--- tic_tac_toe_starter/app.js
+++ tic_tac_toe_starter/app.js
@@ -3,26 +3,23 @@
  * Description: A simple Tic-Tac-Toe game
  * Author: [Elen Francesca]
  **********************************************/
-
 // Select the status display element from the DOM.
 // We'll use this to display messages to the user.
 const statusDisplay = document.querySelector(".game--status");
 
 // Set initial game state values
-let gameActive = true; // This keeps track of whether the game is active or has ended
-let currentPlayer = "X"; // This tracks whose turn it currently is
-let gameState = ["", "", "", "", "", "", "", "", ""]; // Represents the 9 cells in the game board
-
-// A function to return the current player's turn message
-const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
-
-// Display the initial status message in the DOM
-statusDisplay.innerHTML = currentPlayerTurn();
+let gameActive = true;// This keeps track of whether the game is active or has ended
+let counter = 0;
+let currentPlayer = "X";// This tracks whose turn it currently is
+let gameState = ["", "", "", "", "", "", "", "", ""];// Represents the 9 cells in the game board
 
 // Define the possible winning conditions for Tic-Tac-Toe
 // Each array within this array represents a set of indices in 'gameState'
 // that forms a winning line
 const winningConditions = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6] ];
+
+// A function to return the current player's turn message
+const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
 
 /**
  * handleCellPlayed
@@ -35,27 +32,29 @@ const winningConditions = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8],
 // Display the current player's symbol in the clicked cell
 
 const cells = document.getElementsByClassName('cell');
+const restartBtn = document.querySelector('.game--restart');
+restartBtn.addEventListener('click',handleRestartGame);
+
+// Display the initial status message in the DOM
+statusDisplay.innerHTML = currentPlayerTurn();
 
 for(let cell of cells){
+
+    console.log('\n We are  in checking in which cell we are\n')
+
     cell.addEventListener("click", function(event){
+
         let clickedCellIndex = event.target.getAttribute('data-cell-index');
+
+        console.log('event.target:',event.target, '\nclicked Cell Index:',clickedCellIndex, `call handleCellPlayed` );
+
         handleCellPlayed(event.target, clickedCellIndex);
+
+        console.log('Called handleCellPlayed from cell listener was successfulll finished  ');
+
     })
 }
 
-function handleCellPlayed(clickedCell, clickedCellIndex) {
-    if(gameState[clickedCellIndex]===''){
-        gameState[clickedCellIndex] = currentPlayer;
-        clickedCell.innerHTML = currentPlayer;
-        // handlePlayerChange();
-        handleResultValidation();
-    }else{
-        statusDisplay.innerHTML ='This cell is occupied, try another one';
-        setTimeout(() => {
-            statusDisplay.innerHTML = currentPlayerTurn()
-        },2000);
-    }
-}
 
 /**
  * handlePlayerChange
@@ -63,18 +62,31 @@ function handleCellPlayed(clickedCell, clickedCellIndex) {
  * Switches the active player from X to O or O to X.
  * Also updates the UI text to notify whose turn it is.
  */
-function handlePlayerChange() {
+function handleCellPlayed(clickedCell, clickedCellIndex){
+    console.log('\nIn handleCellPlayed\n');
 
-    // Toggle the current player
-    // Update the status text to reflect the new player's turn
-    if(currentPlayer === "X"){
-        currentPlayer = "0"
-        console.log(currentPlayer);
+    if (gameState[clickedCellIndex]===""){
+
+        console.log('In handle Cell Played in first If')
+
+        gameState[clickedCellIndex] = currentPlayer;
+        counter+=1;
+
+        clickedCell.innerHTML = currentPlayer;
+
+        console.log('currentPlayer:', currentPlayer, '\ncall handle Result Validation()');
+
+        handleResultValidation();
+
     }else{
-        currentPlayer = "X";
+        console.log('We are in occupied cell')
+
+        statusDisplay.innerHTML ='This cell is occupied, try another one';
+
+        setTimeout(() => {
+            statusDisplay.innerHTML = currentPlayerTurn()
+        },2000);
     }
-    statusDisplay.innerHTML = currentPlayerTurn()
-    console.log(gameState[winningConditions[0][0]])
 }
 
 /**
@@ -85,28 +97,9 @@ function handlePlayerChange() {
  * If a draw, display a draw message and end the game.
  * Otherwise, switch players.
  */
-function handleResultValidation() {
-    let roundWon = false;
-
-    // winningConditions = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6] ];
-
+function handleResultValidation(){
     // Iterate through each winning condition
-    for(let i = 0; i < winningConditions.length-1; i++){
-        let j=0
-        console.log('check status first:',gameState[winningConditions[i][j]])
-        if(gameState[winningConditions[i][j]]===gameState[winningConditions[i][j+1]]){
-            j=j+1
-            if(gameState[winningConditions[i][j]]===gameState[winningConditions[i][j+1]]){
-                roundWon = true;
-                console.log('wiiiiin')
-            }
-        }
-    }
-    if(roundWon){
 
-    }else{
-        handlePlayerChange()
-    }
     // Destructure the three cell indices that form a potential win
 
     // If any cell is empty, skip this iteration
@@ -118,30 +111,45 @@ function handleResultValidation() {
     // If there are no empty cells in 'gameState', it's a draw
 
     // If the game is neither won nor drawn, switch to the next player
-    handlePlayerChange();
+
+    console.log('\nIn handleResultValidation\n');
+
+    for(let i = winningConditions.length-1; i>=0; i--){
+        let a = winningConditions[i][0];
+        let b = winningConditions[i][1];
+        let c = winningConditions[i][2];
+
+        console.log('\na:', a, '\nb:', b, '\nc:', c);
+
+        if(gameState[a]!=='' && gameState[b]!=='' &&gameState[c]!=='' && gameState[a] === gameState[b] && gameState[a] === gameState[c]){
+            console.log(currentPlayer +' win');
+            statusDisplay.innerHTML = currentPlayer + ' WIN!';
+            gameActive = false;
+        }else if(counter===9) {
+            console.log('It is a draw');
+            statusDisplay.innerHTML = 'Draw!'
+            gameActive = false;
+        }
+    }
+    if (gameActive) {
+        handlePlayerChange();
+    }else{
+        setTimeout(() => {
+            handleRestartGame();
+        },2000);
+    }
 }
 
-/**
- * handleCellClick
- * ---------------
- * This function is triggered whenever a cell in the board is clicked.
- * It determines which cell was clicked, checks if that cell is already used
- * or if the game is inactive, and if valid, calls the functions to update the game state.
- * @param {Event} clickedCellEvent - The click event on one of the cells.
- */
-function handleCellClick(clickedCellEvent) {
-    // The clicked cell element
-
-    // The index of the cell based on its data attribute
-    const clickedCellIndex = parseInt(
-        clickedCell.getAttribute("data-cell-index")
-    );
-
-    // If the cell is already filled or the game is not active, don't do anything
-
-    // Otherwise, handle the cell being played and validate results
-
-    handleResultValidation();
+function handlePlayerChange (){
+    console.log('\nin handlePlayerChange\n');
+    if(currentPlayer === "X"){
+        currentPlayer = "0"
+        console.log('currentPlayer:',currentPlayer);
+    }else{
+        currentPlayer = "X";
+        console.log('currentPlayer:',currentPlayer);
+    }
+    statusDisplay.innerHTML = currentPlayerTurn();
 }
 
 /**
@@ -154,15 +162,20 @@ function handleCellClick(clickedCellEvent) {
  *  - Sets the current player to X
  *  - Updates the status display
  */
+
 function handleRestartGame() {
     gameActive = true;
     currentPlayer = "X";
     gameState = ["", "", "", "", "", "", "", "", ""];
     statusDisplay.innerHTML = currentPlayerTurn();
+    counter = 0;
+
+    for(let i = 0; i < cells.length; i++ ){
+        cells[i].innerHTML = '';
+    }
 
     // Clear each cell in the UI
 }
-
 // Add event listeners to each cell for a click event
 
 // Add event listener to the restart button
